5869b44d63d26c3d4823b627325cdc43ec7cf1c1
Dyaman99 <dyaman@ualr.edu>
2026-02-19 17:22:30 +0000

    added scroll to top button

--- state-parks/assets/js/app.js
+++ state-parks/assets/js/app.js
@@ -13,4 +13,27 @@ $(document).ready(function() {
     useAutoplayToggleButton: false  // removes the pause/play button
 
   });
-});
+
+
+  const scrollTop = document.getElementById("scrollToTopBtn");
+
+  window.addEventListener("scroll", function(){
+    if(window.pageYOffset > 200){
+      scrollTop.style.display="block";
+    }else{
+      scrollTop.style.display="none";
+    }
+  });
+
+  scrollTop.addEventListener("click", function(){
+    window.scrollTo({
+      top:0, 
+      behavior:"smooth"
+    })
+
+  });
+
+}); 
+ 
+
+
